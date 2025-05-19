df8e63e3d771fbe30819e873741442f3eb01ba90
Marc Duiker <marcduiker@users.noreply.github.com>
2025-05-19 13:32:11 +0200

    Install uv via script
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,3 +1,6 @@
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
 # Install the Dapr CLI
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
 
